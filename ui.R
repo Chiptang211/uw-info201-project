bfb84798b46b2aa689e9fb982107c586a52e6a77
Ethan Sheynblat <ethansheynblat@Ethans-MacBook-Pro.local>
2021-06-01 13:16:04 -0700

    added different tabs

--- ui.R
+++ ui.R
@@ -2,24 +2,56 @@ library(shiny)
 
 shinyUI(
     fluidPage(
+      tabPanel(
+        "Component 1",
         titlePanel("Stroke likelihood"),
         sidebarLayout(
-            sidebarPanel(
-                sliderInput(),
-                radioButtons('Inputs', label = 'Demographic Choices', choices = list("ever_married" = c("Yes", "No"), 
-                                                                                     "gender" = c("Male", "Female", "Other"),
-                                                                                     "hypertension" = c("0", "1"), 
-                                                                                     "smoking_status" = c("formerly smoked", "never smoked", 
-                                                                                                          "smokes", "unknown"),
-                                                                                     "work_type" = c("children", "Govt_job", "Self-employed",
-                                                                                                     "Private", "Never_worked"),
-                                                                                     "Residence_type" = c("Urban", "Rural"))),
-                uiOutput()
-            )
+          sidebarPanel(
+            sliderInput(),
+            radioButtons('Inputs', label = 'Demographic Choices', choices = list("ever_married" = c("Yes", "No"), 
+                                                                                 "gender" = c("Male", "Female", "Other"),
+                                                                                 "hypertension" = c("0", "1"), 
+                                                                                 "smoking_status" = c("formerly smoked", "never smoked", 
+                                                                                                      "smokes", "unknown"),
+                                                                                 "work_type" = c("children", "Govt_job", "Self-employed",
+                                                                                                 "Private", "Never_worked"),
+                                                                                 "Residence_type" = c("Urban", "Rural"))),
+            uiOutput()
+          )
         ),
         mainPanel(
-            plotOutput(),
-            textOutput()
+          plotOutput(),
+          textOutput()
         )
+      ),
+      tabPanel(
+        "Component 2",
+        titlePanel(),
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput(),
+            radioButtons(),
+            uiOutput()
+          )
+        ),
+        mainPanel(
+          plotOutput(),
+          textOutput()
+        )
+      ),
+      tabPanel(
+        "Component 3",
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput(),
+            radioButtons(),
+            uiOutput()
+          )
+        ),
+        mainPanel(
+          plotOutput(),
+          textOutput()
+        )
+      ),
     )
 )
