21bdb922fb27335dae31b3b3fa2e222b810f431b
Ethan Sheynblat <ethansheynblat@Ethans-MacBook-Pro.local>
2021-06-01 14:28:27 -0700

    added filters for graphs and radio choices

--- server.R
+++ server.R
@@ -5,5 +5,22 @@ library(dplyr)
 data <- read.csv("healthcare-dataset-stroke-data.csv")
 
 shinyServer(function(input, output) {
+  displayGraph <- reactive({
+    graph <- data %>%
+      select(age, stroke,hypertension,ever_married,gender,smoking_status,work_type,Residence_type)%>%
+      group_by(age)%>%
+      filter(stroke == "1")%>%
+      filter(hypertension %in% input$hypertension)%>%
+      filter(ever_married %in% input$ever_married)%>%
+      filter(gender %in% input$gender)%>%
+      filter(smoking_status %in% input$smoking_status)%>%
+      filter(work_type %in% input$work_type)%>%
+      filter(Residence_type %in% input$Residence_type)%>%
+      mutate(totals_strokes = n())
+  })
   
+  output$disPlot <- renderPlot({
+    ggplot(displayGraph(), aes(age, totals)) +
+      geom_line()
+  })
 })
