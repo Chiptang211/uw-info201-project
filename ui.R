92c152a95bfe14727af4285cf7d34c0d68b4e3d4
Youri Babakoff <youribabakoff@hotmail.com>
2021-06-02 14:13:07 -0700

    Updated first dashboard. OutputId error.

--- ui.R
+++ ui.R
@@ -6,21 +6,25 @@ shinyUI(
         "Component 1",
         titlePanel("Stroke likelihood"),
         sidebarLayout(
-          sidebarPanel(
-            sliderInput(),
-            radioButtons('Inputs', label = 'Demographic Choices', choices = list("ever_married" = c("Yes", "No"), 
-                                                                                 "gender" = c("Male", "Female", "Other"),
-                                                                                 "hypertension" = c("0", "1"), 
-                                                                                 "smoking_status" = c("formerly smoked", "never smoked", 
-                                                                                                      "smokes", "unknown"),
-                                                                                 "work_type" = c("children", "Govt_job", "Self-employed",
-                                                                                                 "Private", "Never_worked"),
-                                                                                 "Residence_type" = c("Urban", "Rural"))),
-            uiOutput()
-          )
+          sidebarPanel,
+          mainPanel,
+          position = c("left", "right"),
+          fluid = TRUE
+        ),
+        
+        sidebarPanel(
+          radioButtons('Inputs', label = 'Demographic Choices', choices = list("ever_married" = c("Yes", "No"), 
+                                                                               "gender" = c("Male", "Female", "Other"),
+                                                                               "hypertension" = c("0", "1"), 
+                                                                               "smoking_status" = c("formerly smoked", "never smoked", 
+                                                                                                    "smokes", "unknown"),
+                                                                               "work_type" = c("children", "Govt_job", "Self-employed",
+                                                                                               "Private", "Never_worked"),
+                                                                               "Residence_type" = c("Urban", "Rural"))),
+          uiOutput()
         ),
         mainPanel(
-          plotOutput(),
+          plotOutput("disPlot"),
           textOutput()
         )
       ),
