1958c6195f581d34fc291b455c4dbd5a2c1789a9
Ethan Sheynblat <ethansheynblat@Ethans-MacBook-Pro.local>
2021-06-05 10:41:21 -0700

    almost finished app

--- server.R
+++ server.R
@@ -2,7 +2,7 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-data <- read.csv("healthcare-dataset-stroke-data.csv")
+data <- read.csv("~/Desktop/Info201/uw-info201-project/healthcare-dataset-stroke-data.csv")
 
 shinyServer(function(input, output) {
   displayGraph <- reactive({
@@ -10,33 +10,48 @@ shinyServer(function(input, output) {
       select(age, stroke,hypertension,ever_married,gender,smoking_status,work_type,Residence_type,bmi,avg_glucose_level)%>%
       group_by(age)%>%
       filter(stroke == "1")%>%
-      filter(hypertension %in% input$hypertension)%>%
-      filter(ever_married %in% input$ever_married)%>%
       filter(gender %in% input$gender)%>%
       filter(smoking_status %in% input$smoking_status)%>%
-      filter(work_type %in% input$work_type)%>%
-      filter(Residence_type %in% input$Residence_type)%>%
       mutate(totals_strokes = n())
   })
-
+  
   #builds the first graph based on age and # of strokes
   output$disPlot <- renderPlot({
     ggplot(displayGraph(), aes(age, totals_strokes)) +
       geom_line()
   })
-
-  #builds the age versus BMI graph
-  output$ageVsBmi <- renderPlot({
-    ggplot(displayGraph(), aes(x=age, y=bmi)) +
-      geom_point(size=2, shape=21 )
+  
+  
+  bmiAndglucose <- reactive({
+    if(input$graph_choice == "AgevsBMI"){
+      graph1 <- data%>%
+        select(age,bmi,stroke,gender,smoking_status)%>%
+        group_by(age)%>%
+        filter(stroke == "1")%>%
+        filter(gender %in% input$gender_choice)%>%
+        filter(smoking_status %in% input$smoking_status_choice)
+    }else{
+      graph2 <- data%>%
+        select(age,avg_glucose_level, stroke,gender,smoking_status)%>%
+        group_by(age)%>%
+        filter(stroke == "1")%>%
+        filter(gender %in% input$gender_choice)%>%
+        filter(smoking_status %in% input$smoking_status_choice)  
+    }
   })
-
-  #builds the age versus glucose level graph
-  output$ageVsGlucose <- renderPlot({
-    ggplot(displayGraph(), aes(x=age, y=avg_glucose_level)) +
-      geom_point(size=2, shape=21)
+  
+  output$bmiAndglucoseGraph <- renderPlot({
+    if(input$graph_choice == "AgevsBMI"){
+      ggplot(bmiAndglucose(), aes(x = age, y = bmi))+
+        geom_point(size = 2, shape = 21)+
+        geom_smooth(method = "loess", se = F)
+    }else{
+      ggplot(bmiAndglucose(), aes(x = age, y = avg_glucose_level))+
+        geom_point(size = 2, shape = 21)+
+        geom_smooth(method = "loess", se = F)
+    }
   })
-
+  
   output$myImage <- renderImage({
     # A temp file to save the output.
     # This file will be removed later by renderImage
@@ -47,5 +62,22 @@ shinyServer(function(input, output) {
          height = 60,
          alt = "a diagram of a stroke")
   }, deleteFile = FALSE)
-
+  
+  heartdiseaseVsstroke <- reactive({
+    graph3 <- data %>%
+      select(heart_disease,stroke,age, hypertension,ever_married,gender,smoking_status,work_type,Residence_type)%>%
+      group_by(age)%>%
+      filter(hypertension %in% input$hypertension_1)%>%
+      filter(ever_married %in% input$ever_married_1)%>%
+      filter(gender %in% input$gender_1)%>%
+      filter(smoking_status %in% input$smoking_status_1)%>%
+      filter(work_type %in% input$work_type_1)%>%
+      filter(Residence_type %in% input$Residence_type_1)
+  })
+  
+  output$heartdiseaseVsstrokeGraph <- renderPlot({
+    ggplot(heartdiseaseVsstroke(), aes(heart_disease,stroke))+
+      geom_histogram(stat = "identity")
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -5,117 +5,89 @@ library(imager)
 im <- load.image("strokeimg.jpg")
 
 
-shinyUI(navbarPage("MyApp",
+shinyUI(navbarPage("Stroke Susceptibility",
                    tabPanel(
                      p("Introduction"),
                      p("This project analyzes a stroke dataset to help users understand whether they are at risk for strokes and how they can mitigate their risks. Using several visual representations, users will be able to determine which factors contribute the most to strokes and what they need to focus on. The dataset we are working with is Kaggle's Stroke Prediction Dataset. The source of the data is confidential for patient confidentiality, but the author, a data scientist from Madrid, Spain, has earned several awards for being a datasets expert. This dataset has earned a gold ranking on Kaggle's website for being one of the best and most reliable datasets on the site."),
                      imageOutput("myImage"),
                    ),
                    tabPanel(
-                     "Component 1",
-                     titlePanel("Stroke likelihood"),
+                     "Total Strokes",
+                     titlePanel("Stroke Data"),
                      sidebarPanel(
-                       checkboxGroupInput('ever_married', label = 'Married', choices = list("Yes"="Yes",
-                                                                                            "No"="No"),
-                                          selected = "True"),
                        checkboxGroupInput('gender', label = 'Gender', choices = list("Male"="Male",
                                                                                      "Female"="Female",
                                                                                      "Other"="Other"),
                                           selected = "Other"),
-                       checkboxGroupInput('hypertension', label = 'Hypertension', choices = list("Yes"=1,
-                                                                                                 "No"=0),
-                                          selected = 1),
                        checkboxGroupInput('smoking_status', label = 'Smoking', choices = list("Formerly"="formerly smoked",
                                                                                               "Never"="never smoked",
                                                                                               "Smokes"="smokes",
                                                                                               "Unknown"="Unknown"),
-                                          selected = "smokes"),
-                       checkboxGroupInput('work_type', label = 'Work Type', choices = list("Children"="children",
-                                                                                           "Government"="Govt_job",
-                                                                                           "Never Worked"="Never_worked",
-                                                                                           "Private"="Private",
-                                                                                           "Self-employed"="Self-employed"),
-                                          selected = "Private"),
-                       checkboxGroupInput('Residence_type', label = 'Residence Type', choices = list("Rural"="Rural",
-                                                                                                     "Urban"="Urban"),
-                                          selected = "Urban"),
+                                          selected = "smokes")
                      ),
-
+                     
                      mainPanel(
                        plotOutput("disPlot")
                      ),
                      p("This chart shows the age as compared to the amount of strokes that people have had, as an average, in that age group."),
                      p("While being at an age where a stroke is more likely to occur does not guarantee a stroke, it is a solid indicator that it is a larger issue  than it would be for younger age groups."),
-
+                     
                    ),
                    tabPanel(
-                     "Component 2",
+                     "Age vs. BMI/Glucose",
                      titlePanel("Stroke likelihood"),
                      sidebarPanel(
-                       checkboxGroupInput('ever_married', label = 'Married', choices = list("Yes"="Yes",
-                                                                                            "No"="No"),
-                                          selected = "Yes"),
-                       checkboxGroupInput('gender', label = 'Gender', choices = list("Male"="Male",
-                                                                                     "Female"="Female",
-                                                                                     "Other"="Other"),
+                       selectInput('graph_choice', label = 'BMI or Glucose', choices = list("AgevsBMI" = "AgevsBMI",
+                                                                                            "AgevsGlucose" = "AgevsGlucose")),
+                       checkboxGroupInput('gender_choice', label = 'Gender', choices = list("Male"="Male",
+                                                                                            "Female"="Female",
+                                                                                            "Other"="Other"),
                                           selected = "Other"),
-                       checkboxGroupInput('hypertension', label = 'Hypertension', choices = list("Yes"=1,
-                                                                                                 "No"=0),
-                                          selected = 1),
-                       checkboxGroupInput('smoking_status', label = 'Smoking', choices = list("Formerly"="formerly smoked",
-                                                                                              "Never"="never smoked",
-                                                                                              "Smokes"="smokes",
-                                                                                              "Unknown"="Unknown"),
-                                          selected = "smokes"),
-                       checkboxGroupInput('work_type', label = 'Work Type', choices = list("Children"="children",
-                                                                                           "Government"="Govt_job",
-                                                                                           "Never Worked"="Never_worked",
-                                                                                           "Private"="Private",
-                                                                                           "Self-employed"="Self-employed"),
-                                          selected = "Private"),
-                       checkboxGroupInput('Residence_type', label = 'Residence Type', choices = list("Rural"="Rural",
-                                                                                                     "Urban"="Urban"),
-                                          selected = "Urban"),
+                       checkboxGroupInput('smoking_status_choice', label = 'Smoking', choices = list("Formerly"="formerly smoked",
+                                                                                                     "Never"="never smoked",
+                                                                                                     "Smokes"="smokes",
+                                                                                                     "Unknown"="Unknown"),
+                                          selected = "smokes")
                      ),
-
+                     
                      mainPanel(
-                       plotOutput("ageVsBmi")
+                       plotOutput("bmiAndglucoseGraph"),
                      ),
                      p("This chart specifically focuses down on the BMI compared to age in people that have experienced strokes."),
                      p("The use of this data comes from the averages, in which if a person is below the average BMI at their age group, they have a lesser likelihood of having a stroke as a side effect of a high BMI. "),
                    ),
                    tabPanel(
-                     "Component 3",
-                     titlePanel("Stroke likelihood"),
+                     "Heart Disease vs. Strokes",
+                     titlePanel("Heart Disease vs. Strokes"),
                      sidebarPanel(
-                       checkboxGroupInput('ever_married', label = 'Married', choices = list("Yes"="Yes",
-                                                                                            "No"="No"),
+                       checkboxGroupInput('ever_married_1', label = 'Married', choices = list("Yes"="Yes",
+                                                                                              "No"="No"),
                                           selected = "Yes"),
-                       checkboxGroupInput('gender', label = 'Gender', choices = list("Male"="Male",
-                                                                                     "Female"="Female",
-                                                                                     "Other"="Other"),
+                       checkboxGroupInput('gender_1', label = 'Gender', choices = list("Male"="Male",
+                                                                                       "Female"="Female",
+                                                                                       "Other"="Other"),
                                           selected = "Other"),
-                       checkboxGroupInput('hypertension', label = 'Hypertension', choices = list("Yes"=1,
-                                                                                                 "No"=0),
+                       checkboxGroupInput('hypertension_1', label = 'Hypertension', choices = list("Yes"=1,
+                                                                                                   "No"=0),
                                           selected = 1),
-                       checkboxGroupInput('smoking_status', label = 'Smoking', choices = list("Formerly"="formerly smoked",
-                                                                                              "Never"="never smoked",
-                                                                                              "Smokes"="smokes",
-                                                                                              "Unknown"="Unknown"),
+                       checkboxGroupInput('smoking_status_1', label = 'Smoking', choices = list("Formerly"="formerly smoked",
+                                                                                                "Never"="never smoked",
+                                                                                                "Smokes"="smokes",
+                                                                                                "Unknown"="Unknown"),
                                           selected = "smokes"),
-                       checkboxGroupInput('work_type', label = 'Work Type', choices = list("Children"="children",
-                                                                                           "Government"="Govt_job",
-                                                                                           "Never Worked"="Never_worked",
-                                                                                           "Private"="Private",
-                                                                                           "Self-employed"="Self-employed"),
+                       checkboxGroupInput('work_type_1', label = 'Work Type', choices = list("Children"="children",
+                                                                                             "Government"="Govt_job",
+                                                                                             "Never Worked"="Never_worked",
+                                                                                             "Private"="Private",
+                                                                                             "Self-employed"="Self-employed"),
                                           selected = "Private"),
-                       checkboxGroupInput('Residence_type', label = 'Residence Type', choices = list("Rural"="Rural",
-                                                                                                     "Urban"="Urban"),
+                       checkboxGroupInput('Residence_type_1', label = 'Residence Type', choices = list("Rural"="Rural",
+                                                                                                       "Urban"="Urban"),
                                           selected = "Urban"),
                      ),
-
+                     
                      mainPanel(
-                       plotOutput("ageVsGlucose")
+                       plotOutput("heartdiseaseVsstrokeGraph")
                      ),
                      p("This chart specifically focuses on the glucose levels compared to age in people that have experienced strokes."),
                      p("If someone is below the average you can visually see on the chart, they are less likely to have a stroke as a side effect of their glucose levels. However, being above or below is not an absolute, just an indication."),
@@ -126,5 +98,5 @@ shinyUI(navbarPage("MyApp",
                      p("After having analyzed this dataset, we are satisfied with the quality of the data since it provided many useful insights on the typical stroke patient. Since this sample has such varying cases and people with many different conditions, we believe the data gives unbiased results that are representative of the greater population. We also see no issues with this dataset harming certain population groups because it is exclusive of race and equally represents men and women."),
                      p("In the future, it would be interesting to advance this project by using a larger dataset and seeing whether these factors change based on the country people are from.")
                    )
-
+                   
 ))
